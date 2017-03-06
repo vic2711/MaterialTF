79da9374efc5920f06b8175bc478eb0519051b52
Viktor Miroshnychenko <miroshnichenko27@gmail.com>
2017-03-06 16:35:50 +0200

    First commit

---
+++ FloatingTF/FloatingTF.h
@@ -0,0 +1,19 @@
+//
+//  FloatingTF.h
+//  FloatingTF
+//
+//  Created by Viktor Miroshnychenko on 3/6/17.
+//  Copyright © 2017 vic2711. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingTF.
+FOUNDATION_EXPORT double FloatingTFVersionNumber;
+
+//! Project version string for FloatingTF.
+FOUNDATION_EXPORT const unsigned char FloatingTFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingTF/PublicHeader.h>
+
+
